4937513f8ecec28ac2d126e7036b807220b62e2b
OriginalYan <sokoliko999@gmail.com>
2019-04-20 18:12:48 +0300

    loading on github

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let obj = {
+    name: "Yan",
+    surname: "Sokolikov",
+    lasname: "Vyacheslavovich",
+    getName: function(){
+        return this.name;
+    },
+    getSurname: function(){
+        return this.surname;
+    }
+};
